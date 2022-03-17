36cb32684366f9f42cafe487620553c989cbf4f3
delphython <delphython@gmail.com>
2022-03-17 13:51:16 +0300

    Add put_test_data_to_db function

--- foodplanapp/management/commands/parsing_recipes_website.py
+++ foodplanapp/management/commands/parsing_recipes_website.py
@@ -1,11 +1,40 @@
+import os
+
 import requests
 
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
+from urllib.parse import urljoin, urlparse, unquote
+
+from foodplanapp.models import (
+    Dish,
+    Dishitems,
+)
+
+
+def get_file_name(url):
+    parsed_url = urlparse(url)
+    path = unquote(parsed_url.path)
+    _, file_name = os.path.split(path)
+
+    return file_name
+
+
+def download_image(url, filename, folder="images/"):
+    response = requests.get(url)
+    response.raise_for_status()
+    file_path = os.path.join(folder, filename)
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    return file_path
 
 
 def parse_receipt_pages(response):
     receipts = []
+
+    images_folder_name = os.path.join(os.getcwd(), "images/")
+    os.makedirs(images_folder_name, exist_ok=True)
 
     soup = BeautifulSoup(response.text, "lxml")
 
@@ -23,7 +52,13 @@ def parse_receipt_pages(response):
         receipt_soup = BeautifulSoup(response.text, "lxml")
 
         receipt_name = receipt_soup.select_one("h1").text
-        receipt_image = receipt_soup.select_one("div.m-img img")["src"]
+        image_url = receipt_soup.select_one("div.m-img img")["src"]
+
+        image_file_name = get_file_name(image_url)
+        receipt_image = download_image(
+            image_url, image_file_name, images_folder_name
+        )
+
         receipt_ingredient_names = [
             name.text
             for name in receipt_soup.select(
@@ -47,7 +82,9 @@ def parse_receipt_pages(response):
         nutritional_table = receipt_soup.find("table")
         need_row = nutritional_table.find_all("tr")[1]
         columns = need_row.find_all("td")
-        receipt_nutritional_value = columns[0].find("strong").text.strip()
+        receipt_nutritional_value = (
+            columns[0].find("strong").text.strip().split()[0]
+        )
 
         receipt_guide_rows = receipt_soup.select("div.cooking-bl div p")
         for receipt_guide_row in receipt_guide_rows:
@@ -66,17 +103,36 @@ def parse_receipt_pages(response):
     return receipts
 
 
+def put_test_data_to_db(receipts):
+    for receipt in receipts:
+        dish = Dish.objects.create(
+            name=receipt["receipt_name"],
+            image=receipt["receipt_image"],
+            calories=receipt["receipt_nutritional_value"],
+            guide=receipt["receipt_guide"],
+        )
+        for ingredient in receipts["receipt_ingredients"]:
+            amount, measurement_unit = ingredient[1].split(" ", 1)
+            Dishitems.objects.create(
+                dish=dish,
+                ingredient=ingredient[0],
+                amount=amount,
+                measurement_unit=measurement_unit,
+            )
+
+
 class Command(BaseCommand):
     help = "Парсинг сайта с рецепатами povarenok.ru"
 
     def handle(self, *args, **options):
-        # рецепты супов
-        soup_receipts_url = (
-            "https://www.povarenok.ru/recipes/dishes/first/?searchid=28"
-        )
-        soup_receipts_url_response = requests.get(soup_receipts_url)
-        soup_receipts_url_response.raise_for_status()
+        receipts_urls = [
+            "https://www.povarenok.ru/recipes/dishes/first/?searchid=28",
+        ]
 
-        soup_receipts = parse_receipt_pages(soup_receipts_url_response)
+        for receipts_url in receipts_urls:
+            receipts_url_response = requests.get(receipts_url)
+            receipts_url_response.raise_for_status()
 
-        print(soup_receipts)
+            receipts = parse_receipt_pages(receipts_url_response)
+
+            put_test_data_to_db(receipts)
