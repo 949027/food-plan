353cb92379e9b4ec3c4c4f2f6d22343d570546e7
Yulia51188 <sun51188@gmail.com>
2022-03-17 09:05:18 +0300

    Create new app `accounts`

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "foodplanapp.apps.FoodplanappConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
