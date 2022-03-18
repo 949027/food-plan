2a498526e56a3f8bde1da949fe98aadbe72c9ef7
Yulia51188 <sun51188@gmail.com>
2022-03-18 15:38:34 +0300

    Move auth forms to distinct module

--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,11 @@
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import forms
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label='Логин', max_length=75, required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Укажите имя пользователя'
-        })
-    )
-    password = forms.CharField(
-        label='Пароль', max_length=75, required=True,
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Введите пароль'
-        })
-    )
-
-
-class RegisterationForm(UserCreationForm):
-    username = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))    
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
+from .forms import LoginForm, RegisterationForm
 
 
 class LoginUser(auth_views.LoginView):
@@ -81,4 +51,5 @@ class UserCreateView(CreateView):
 
 @login_required
 def user_profile(request):
-    return render(request, 'lk.html', context={'username': request.user.get_username()})
+    return render(request, 'lk.html', 
+                  context={'username': request.user.get_username()})
