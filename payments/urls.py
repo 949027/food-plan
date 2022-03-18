2bf75e81b87c29d7117c0fcd23063b674fc49af2
Yulia51188 <sun51188@gmail.com>
2022-03-18 17:26:29 +0300

    Handle errors while payment redirect

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path('<int:order_id>/', views.payment, name='payment'),
     path('complete/<int:order_id>/', views.complete_payment, name='complete_payment'),
--- payments/views.py
+++ payments/views.py
@@ -1,38 +1,49 @@
-from django.shortcuts import redirect
-from django.urls import reverse
-from yookassa import Configuration, Payment
-from django.conf import settings
 import uuid
 from urllib.parse import urljoin
 
-from .models import OrderPayment
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from yookassa import Configuration, Payment
+from yookassa.domain.exceptions.unauthorized_error import UnauthorizedError
 
 from foodplanapp.models import Order
 
+from .models import OrderPayment
 
+
+@login_required
 def payment(request, order_id):
-    Configuration.account_id = settings.SHOP_ID
-    Configuration.secret_key = settings.SHOP_TOKEN
-    order = Order.objects.get(id=order_id)
+    order = get_object_or_404(Order, id=order_id)
+    if order.payments.filter(is_paid=True).exists():
+        return HttpResponse(f"Заказ {order_id} уже оплачен")
     
     return_url = urljoin(
         request.build_absolute_uri(),
         reverse(complete_payment, kwargs={"order_id": order_id})
     )
 
-    payment = Payment.create({
-        "amount": {
-            "value": order.total_price,
-            "currency": "RUB"
-        },
-        "confirmation": {
-            "type": "redirect",
-            "return_url": return_url,
-        },
-        "capture": True,
-        "description": f"Заказ №{order_id}",
-        "metadata": {"order_id": order_id},
-    }, uuid.uuid4())
+    try:
+        Configuration.account_id = settings.SHOP_ID
+        Configuration.secret_key = settings.SHOP_TOKEN
+
+        payment = Payment.create({
+            "amount": {
+                "value": order.total_price,
+                "currency": "RUB"
+            },
+            "confirmation": {
+                "type": "redirect",
+                "return_url": return_url,
+            },
+            "capture": True,
+            "description": f"Заказ №{order_id}",
+            "metadata": {"order_id": order_id},
+        }, uuid.uuid4())
+    except UnauthorizedError as error:
+        return HttpResponse(f"Ошибка авторизации в форме оплаты, обратитесь в тех. поодержку сайта: {error}")
 
     OrderPayment.objects.create(
         payment_id=payment.id,
@@ -48,6 +59,7 @@ def payment(request, order_id):
     return redirect(payment.confirmation.confirmation_url)
 
 
+@login_required
 def complete_payment(request, order_id):
     order_payment = Order.objects.get(id=order_id).payments.first()
     
