75597c81a59d8e04a8ae00c20c9ad265c2d176bf
Yulia51188 <sun51188@gmail.com>
2022-03-18 15:41:07 +0300

    Format code in accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.forms import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, forms
 from django.contrib.auth.models import User
 
 
@@ -21,10 +20,22 @@ class LoginForm(forms.Form):
 
 
 class RegisterationForm(UserCreationForm):
-    username = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))    
+    username = forms.CharField(
+        label='Имя', 
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.CharField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )    
 
     class Meta:
         model = User
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import views as auth_views
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import LoginForm, RegisterationForm
