774fd4a753b7710cbe4ba6dd985a1d8ea31c502c
delphython <delphython@gmail.com>
2022-03-20 17:29:31 +0300

    Add order information showing

--- accounts/views.py
+++ accounts/views.py
@@ -6,22 +6,20 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import LoginForm, RegistrationForm
+from foodplanapp.models import Dish, Order
 
 
 class LoginUser(auth_views.LoginView):
-
     def get(self, request, *args, **kwargs):
         form = LoginForm()
-        return render(request, "accounts/auth.html", context={
-            'form': form
-        })
+        return render(request, "accounts/auth.html", context={"form": form})
 
     def post(self, request):
         form = LoginForm(request.POST)
 
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
 
             user = authenticate(request, username=username, password=password)
             if user:
@@ -30,26 +28,53 @@ class LoginUser(auth_views.LoginView):
 
 
 class LogoutUser(auth_views.LogoutView):
-    next_page = reverse_lazy('start_page')
+    next_page = reverse_lazy("start_page")
 
 
 class UserCreateView(CreateView):
     form_class = RegistrationForm
-    template_name = 'accounts/registration.html'
+    template_name = "accounts/registration.html"
 
     def __init__(self):
         self.object = None
 
     def get_success_url(self):
-        return reverse_lazy('login')
+        return reverse_lazy("login")
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('profile')
+        return redirect("profile")
 
 
 @login_required
 def user_profile(request):
-    return render(request, 'accounts/lk.html', 
-                  context={'username': request.user.get_username()})
+    order = (
+        Order.objects.prefetch_related("allergies")
+        .filter(user__id=request.user.id)
+        .first()
+    )
+
+    # dish = (
+    #     Dish.objects.prefetch_related("allergies")
+    #     .prefetch_related("dishitems")
+    #     .filter(menu_type=order[0].menu_type)
+    #     .exclude(allergies__in=allergies)
+    #     .order_by("?")
+    #     .first()
+    # )
+    allergies = list(
+        order.allergies.values(
+            "name",
+        )
+    )
+
+    return render(
+        request,
+        "accounts/lk.html",
+        context={
+            "user": request.user,
+            "order": order,
+            "allergies": allergies,
+        },
+    )
