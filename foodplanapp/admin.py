e197b33f8db0a84d7cd1dfccf843c1a085dc6180
Yulia51188 <sun51188@gmail.com>
2022-03-21 13:48:04 +0300

    Update Order admin

--- foodplanapp/admin.py
+++ foodplanapp/admin.py
@@ -10,7 +10,7 @@ class DishitemsInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'total_price', 'user',)
 
 
 @admin.register(Price)
