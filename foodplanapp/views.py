6aecbe29b71a5acade01f0a34164f0e9b298402f
949027 <82878668+949027@users.noreply.github.com>
2022-03-21 09:22:02 +0700

    Fixed order creation. Added promocode

--- foodplanapp/views.py
+++ foodplanapp/views.py
@@ -1,9 +1,10 @@
+from datetime import date
 from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from .forms import OrderForm
-from .models import Price, Dish, Order
+from .models import Price, Dish, Order, Promocode
 
 
 @login_required
@@ -23,23 +24,39 @@ def order(request):
                 start_price = order.duration * price.vegan_menu
             elif order.menu_type == "keto":
                 start_price = order.duration * price.keto_menu
-            total_price = (
-                start_price
-                + (order.breakfast * price.meal)
-                + (order.lunch * price.meal)
-                + (order.dinner * price.meal)
-                + (order.dessert * price.meal)
-                + (order.new_year_menu * price.new_year_menu)
-                + (order.allergy1 * price.allergy)
-                + (order.allergy2 * price.allergy)
-                + (order.allergy3 * price.allergy)
-            )
+            total_price = sum([
+                start_price,
+                (order.breakfast * price.meal),
+                (order.lunch * price.meal),
+                (order.dinner * price.meal),
+                (order.dessert * price.meal),
+                (order.new_year_menu * price.new_year_menu),
+                (order.allergy1 * price.allergy),
+                (order.allergy2 * price.allergy),
+                (order.allergy3 * price.allergy),
+            ])
+            if order.promo_code:
+                if Promocode.objects.filter(code=order.promo_code.upper()):
+                    promo_code = Promocode.objects.get(
+                        code=order.promo_code.upper()
+                    )
+                    if promo_code.valid_from < date.today() < promo_code.valid_to:
+                        total_price -= promo_code.discount
+                else:
+                    order.promo_code = None
+
+            Order.objects.all().delete()
             order.total_price = total_price
             order.save()
         return render(
             request, 
             "order.html", 
-            {"form": form, "total_price": total_price, "order_id": order.id}
+            {
+                "form": form,
+                "total_price": total_price,
+                "order_id": order.id,
+                "promo_code": order.promo_code
+            }
         )
 
     form = OrderForm()
